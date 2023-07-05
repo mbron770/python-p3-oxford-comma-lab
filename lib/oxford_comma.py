126f331974eb9d66a03334a4e32470faaec6d42c
mbron770 <mbronfin.tsd@gmail.com>
2023-07-04 23:04:20 -0400

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,6 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2: 
+        items[-1] = 'and ' + items[-1]
+    return ', '.join(items) if len(items) > 2 else ' and '.join(items)
+     
+    
